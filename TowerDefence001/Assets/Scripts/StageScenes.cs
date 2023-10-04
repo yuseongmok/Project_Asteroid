a3915a3279674a3bded605dc29fdc915aabf4abd
yuseongmok <202313006@g.shingu.ac.kr>
2023-10-05 01:55:08 +0900

    Feat: "스테이지씬 제작"
    SMS, 스테이지씬 제작
    
    Resolves: #123
    Ref: #456
    Related to: #48, #45

--- TowerDefence001/Assets/Scripts/StageScenes.cs
+++ TowerDefence001/Assets/Scripts/StageScenes.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NextScenes : MonoBehaviour
+public class StageScenes : MonoBehaviour
 {
     public int targetSceneIndex; // 전환할 다음 씬의 인덱스를 인스펙터에서 설정할 수 있도록 변수를 선언합니다.
 
     public void LoadNextScene()
     {
-        LoadingScene.LoadScene("Play");
+        LoadingScene.LoadScene("Stage");
     }
 }
